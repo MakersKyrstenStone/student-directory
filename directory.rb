ca2894abf45f12a624373d72aaaedab20d0acad0
Kyrsten <Kyrsten>
2022-01-14 15:07:20 +0000

    update1directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 5
+# first print out list of students
 puts "The students of Villians Academy"
 puts "--------------"
 puts "The Borg"
@@ -5,6 +7,9 @@ puts "Darth Vader"
 puts "Terminator"
 puts "The Wicked Witch of indetermanite origin"
 puts "The Dragon from that film"
-print "overall we have "
-print 5
-print " naughty students"
+# then print out the number of students
+# print "overall we have "
+# # its important print doesnot add new lines
+# print student_count
+# print " naughty students"
+puts "Overall we have #{student_count} naughty students"
