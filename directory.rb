1c7efb2e8dd78df2afe473c64f657bf85bedbd4c
Kyrsten <Kyrsten>
2022-01-14 15:18:39 +0000

    arrayandloopadded

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
-student_count = 5
 # first print out list of students
+students = [
+  "The Borg",
+  "Darth Vader",
+  "Terminator",
+  "The Wicked Witch of indetermanite origin",
+  "The Dragon from that film"
+]
 puts "The students of Villians Academy"
 puts "--------------"
-puts "The Borg"
-puts "Darth Vader"
-puts "Terminator"
-puts "The Wicked Witch of indetermanite origin"
-puts "The Dragon from that film"
-# then print out the number of students
-# print "overall we have "
-# # its important print doesnot add new lines
-# print student_count
-# print " naughty students"
-puts "Overall we have #{student_count} naughty students"
+students.each do |student|
+  puts student
+end
+# finally we print the total
+puts "Overall we have #{students.count} naughty students"
