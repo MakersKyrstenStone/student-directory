a88f4af0ae03fb907b7a07eea08654d25f55d759
Kyrsten <Kyrsten>
2022-01-16 10:36:54 +0000

    interactivemenubasic

--- directory.rb
+++ directory.rb
@@ -8,6 +8,31 @@
 #   {name: "A very bad man", cohort: :january}
 # ]
 
+def interactive_menu
+  students = []
+  loop do
+    # prints a menu to give user options
+    puts "1. Input student /s to list"
+    puts "2. Show the list of students"
+    puts "9. Exit" # more items to come later
+    # read input and save to a variable
+    selection = gets.chomp
+    # do what user selected
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # causes program to end
+    else
+      puts "Not sure what that is, Pick again"
+    end
+  end
+end
+
 def input_students
   puts "Enter the names of the students"
   puts "to finish enter a blank name"
@@ -37,7 +62,7 @@ def print_footer(names) # finally we print the total
   puts "\nOverall we have #{names.count} naughty students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+students = interactive_menu
+# print_header
+# print(students)
+# print_footer(students)
